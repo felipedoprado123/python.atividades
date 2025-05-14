ffe60dca8249dc9a4ce2bdfe0e99865b4780e420
felipedoprado123 <felipe_do-prado@estudante.sesisenai.org.br>
2025-05-14 11:24:08 +0000

    update

---
+++ lista1/atividade13.2.py
@@ -0,0 +1,10 @@
+soma = 0
+n = 3
+
+for i in range (1, n + 1):
+    Numero = float(input(f"Digite {i}º número: "))
+    soma += numero
+
+mul = soma/n
+
+print("A Média dos nunmmeros digitados é:", mul)
