a20aec4bd35b7d1e1ab7e85dd9d246d873cadfe2
felipedoprado123 <felipe_do-prado@estudante.sesisenai.org.br>
2025-05-14 11:31:28 +0000

    update

--- lista1/atividade13.2.py
+++ lista1/atividade13.2.py
@@ -2,7 +2,7 @@ soma = 0
 n = 3
 
 for i in range (1, n + 1):
-    Numero = float(input(f"Digite {i}º número: "))
+    numero = float(input(f"Digite {i}º número: "))
     soma += numero
 
 mul = soma/n
